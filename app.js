d05a1e96b728b76aecb6622550ffe7b4a36b4fbc
Ankit Sagar <ankit.sagar.me@gmail.com>
2021-07-22 08:37:33 +0530

    login page --design -c

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+const middleware = require("./middleware");
+const path = require("path");
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+
+const loginRoute = require("./routes/loginRoutes");
+
+app.use("/login", loginRoute);
+
+app.get("/",middleware.requireLogin, (req,res,next)=>{
+
+    var payload = {
+        pageTitle: "Home"
+    }
+    res.status(200).render("home",payload);
+});
